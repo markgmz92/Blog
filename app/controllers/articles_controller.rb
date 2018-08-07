7876a4ac545399809a10c9d598b6b0451717e5b6
markgmz92 <markgmz92@gmail.com>
2018-08-08 00:30:11 +0800

    Made partials of comment form and output, put in created_at to display time created of Article and Comment, added HTTP security - dummy security for now.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-
+http_basic_authenticate_with name: "markgmz92", password: "markgmz@92",
+except: [:index, :show]
   def index
     @articles = Article.all
   end
