691a7ead545ec7a3d813265ae07cfc0c242da92e
markgmz92 <markgmz92@gmail.com>
2018-08-08 05:30:58 +0800

    Added a custom helper method for active links in navigation bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # if an active page color = forestgreen
+  # if not on active page color = white
+  #takes_arguements: 'link', 'what page its on', 'change text color'
+    def active_link_to (nav_link, contrl, action, url, style )
+     if controller_name == contrl && action_name == action
+      return link_to nav_link, url, id: style
+    else
+      return link_to nav_link, url, class: style
+     end
+    end
+
 end
